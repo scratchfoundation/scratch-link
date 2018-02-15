2e22a5248adb86a28cec5b57aaedebb2b81d548a
Christopher Willis-Ford <cwillisf@media.mit.edu>
2018-02-15 10:58:41 -0800

    Add Swifter dependency using SwiftPM

--- Package.swift
+++ Package.swift
@@ -5,15 +5,14 @@ import PackageDescription
 
 let package = Package(
     name: "scratch-connect",
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+	dependencies: [
+        .package(url: "https://github.com/httpswift/swifter.git", from: "1.3.3")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "scratch-connect",
-            dependencies: []),
+            dependencies: ["Swifter"]),
     ]
 )
