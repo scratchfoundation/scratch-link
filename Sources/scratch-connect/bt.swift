61214e0fd34601e84bd84b9e682fed69bc6bd192
Christine Karas <christine.karas@ni.com>
2018-04-10 16:19:44 -0500

    A bit of error handling/prevention

--- Sources/scratch-connect/bt.swift
+++ Sources/scratch-connect/bt.swift
@@ -21,7 +21,7 @@ class SessionStub: NSObject {
 }
 
 class ScratchBT: NSObject, IOBluetoothRFCOMMChannelDelegate, IOBluetoothDeviceInquiryDelegate {
-    private var inquiry: IOBluetoothDeviceInquiry?
+    private var inquiry: IOBluetoothDeviceInquiry
     private var connectedChannel: IOBluetoothRFCOMMChannel?
     private var sequenceId = 0
     private var refcon = 0
@@ -29,38 +29,49 @@ class ScratchBT: NSObject, IOBluetoothRFCOMMChannelDelegate, IOBluetoothDeviceIn
     
     init(forSession session: SessionStub) {
         wss = session
+        inquiry = IOBluetoothDeviceInquiry(delegate: nil)
         super.init()
-        inquiry = IOBluetoothDeviceInquiry(delegate: self)
+        
+        // Cannot access self before call to super.init; register self as delegate to capture inquiry status
+        inquiry.delegate = self
     }
     
-    func scan(inMajorDeviceClass major: Int, inMinorDeviceClass minor: Int) {
-        // TODO validate major & minor device types
-        inquiry?.setSearchCriteria(BluetoothServiceClassMajor(kBluetoothServiceClassMajorAny),
+    func scan(inMajorDeviceClass major: UInt, inMinorDeviceClass minor: UInt) {
+        // see https://www.bluetooth.com/specifications/assigned-numbers/baseband for available device classes
+        // LEGO EV3 is major class toy (8), minor class robot (1)
+        inquiry.setSearchCriteria(BluetoothServiceClassMajor(kBluetoothServiceClassMajorAny),
                                    majorDeviceClass: BluetoothDeviceClassMajor(major),
                                    minorDeviceClass: BluetoothDeviceClassMinor(minor))
-        inquiry?.inquiryLength = 30
-        inquiry?.updateNewDeviceNames = true
-        let inquiryStatus = inquiry?.start()
-        sendWSSResponse(returnCode: inquiryStatus)
+        inquiry.inquiryLength = 30
+        inquiry.updateNewDeviceNames = true
+        let inquiryStatus = inquiry.start()
+        sendWSSResponse(inquiryStatus, returnCode: inquiryStatus)
     }
     
     func connect(toDevice deviceId: String) {
-        inquiry?.stop()
-        let availableDevices = inquiry?.foundDevices()
+        inquiry.stop()
+        let availableDevices = inquiry.foundDevices()
         var deviceNotAvailable = true
         for device in availableDevices! {
-            let bluetoothDevice = device as! IOBluetoothDevice
-            if (bluetoothDevice.addressString == deviceId) {
-                let connectionResult =
-                    bluetoothDevice.openRFCOMMChannelAsync(&connectedChannel, withChannelID: 1, delegate: self)
-                sendWSSResponse(returnCode: connectionResult)
-                deviceNotAvailable = false
-                break
+            if let bluetoothDevice = device as? IOBluetoothDevice {
+                if (bluetoothDevice.addressString == deviceId) {
+                    // consider connecting synchronously for a more accurate success result
+                    let connectionResult =
+                        bluetoothDevice.openRFCOMMChannelAsync(&connectedChannel, withChannelID: 1, delegate: self)
+                    if (connectionResult != kIOReturnSuccess) {
+                        sendWSSResponse("Connection process could not start or channel was not found",
+                                        returnCode: connectionResult)
+                    }
+                    deviceNotAvailable = false
+                    break
+                }
+            } else {
+                print("Non-IOBluetoothDevice returned by IOBluetoothDeviceInquiry")
             }
         }
         if deviceNotAvailable {
             // we cannot connect if we do not know about the device
-            sendWSSResponse(returnCode: IOReturn(1))
+            sendWSSResponse("Device \(deviceId) not found", returnCode: IOReturn(1))
         }
     }
     
@@ -71,16 +82,16 @@ class ScratchBT: NSObject, IOBluetoothRFCOMMChannelDelegate, IOBluetoothDeviceIn
             // release the connected channel and reset reference counter so we can reuse it
             connectedChannel = nil
             refcon = 0
-            sendWSSResponse(returnCode: disconnectionResult)
+            sendWSSResponse(nil, returnCode: disconnectionResult)
         } else {
-            sendWSSResponse(returnCode: IOReturn(1))
+            sendWSSResponse("Cannot disconnect from device that is already not connected", returnCode: IOReturn(1))
         }
     }
     
     func sendMessage(toDevice deviceId: String, message: [UInt8]) {
         let bluetoothDevice = connectedChannel?.getDevice()
         if connectedChannel?.isOpen() == false || bluetoothDevice?.addressString != deviceId {
-            sendWSSResponse(returnCode: IOReturn(1))
+            sendWSSResponse("Device \(deviceId) is not connected", returnCode: IOReturn(1))
             return
         }
         
@@ -89,7 +100,7 @@ class ScratchBT: NSObject, IOBluetoothRFCOMMChannelDelegate, IOBluetoothDeviceIn
         if message.count <= Int(mtu!) {
             let messageResult = connectedChannel?.writeAsync(&data, length: UInt16(message.count), refcon: &refcon)
             if (messageResult != kIOReturnSuccess) {
-                sendWSSResponse(returnCode: messageResult)
+                sendWSSResponse("Failed to buffer message", returnCode: messageResult)
             } // a success here may be false positive, handle in callback
         } else {
             // split it up and send in chunks
@@ -129,7 +140,7 @@ class ScratchBT: NSObject, IOBluetoothRFCOMMChannelDelegate, IOBluetoothDeviceIn
      */
     
     func rfcommChannelOpenComplete(_ rfcommChannel: IOBluetoothRFCOMMChannel!, status error: IOReturn) {
-        wss.writeText("connected with error \(error)")
+        sendWSSResponse(error, returnCode: error)
     }
     
     func rfcommChannelData(_ rfcommChannel: IOBluetoothRFCOMMChannel!,
@@ -159,20 +170,21 @@ class ScratchBT: NSObject, IOBluetoothRFCOMMChannelDelegate, IOBluetoothDeviceIn
     func rfcommChannelWriteComplete(_ rfcommChannel: IOBluetoothRFCOMMChannel!,
                                     refcon: UnsafeMutableRawPointer!,
                                     status error: IOReturn) {
-        sendWSSResponse(returnCode: error) // include number of bytes sent to device
+        let numberOfBytesSent = 100
+        sendWSSResponse(numberOfBytesSent, returnCode: error) // include number of bytes sent to device
     }
     
     /*
      * Helper methods
      */
     
-    func sendWSSResponse(returnCode: IOReturn?) {
+    func sendWSSResponse(_ message: Any?, returnCode: IOReturn?) {
         do {
             if (returnCode == kIOReturnSuccess) {
                 let response: [String: Any] = [
                     "jsonrpc": "2.0",
                     "id": sequenceId,
-                    "result": returnCode
+                    "result": message
                 ]
                 let responseData = try JSONSerialization.data(withJSONObject: response)
                 if let responseString = String(bytes: responseData, encoding: .utf8) {
@@ -183,7 +195,7 @@ class ScratchBT: NSObject, IOBluetoothRFCOMMChannelDelegate, IOBluetoothDeviceIn
                 let response: [String: Any] = [
                     "jsonrpc": "2.0",
                     "id": sequenceId,
-                    "error": returnCode
+                    "error": message
                 ]
                 let responseData = try JSONSerialization.data(withJSONObject: response)
                 if let responseString = String(bytes: responseData, encoding: .utf8) {
