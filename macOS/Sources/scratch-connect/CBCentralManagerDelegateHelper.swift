063ae2b47d9eab9111f4badd6b524e1fa630bd66
Christopher Willis-Ford <cwillisf@media.mit.edu>
2018-05-22 23:56:44 -0700

    Add peripheral delegate helper

--- macOS/Sources/scratch-connect/CBCentralManagerDelegateHelper.swift
+++ macOS/Sources/scratch-connect/CBCentralManagerDelegateHelper.swift
@@ -9,42 +9,58 @@ import CoreBluetooth
 class CBCentralManagerDelegateHelper: NSObject, CBCentralManagerDelegate {
     weak var delegate: SwiftCBCentralManagerDelegate?
 
+    @available(OSX 10.7, *)
     func centralManagerDidUpdateState(_ central: CBCentralManager) {
         delegate?.centralManagerDidUpdateState(central)
     }
 
+    @available(OSX 10.7, *)
     func centralManager(_ central: CBCentralManager, willRestoreState dict: [String: Any]) {
         delegate?.centralManager?(central, willRestoreState: dict)
     }
 
+    @available(OSX 10.7, *)
     func centralManager(_ central: CBCentralManager, didDiscover peripheral: CBPeripheral,
                         advertisementData: [String: Any], rssi RSSI: NSNumber) {
         delegate?.centralManager?(central, didDiscover: peripheral, advertisementData: advertisementData, rssi: RSSI)
     }
 
+    @available(OSX 10.7, *)
     func centralManager(_ central: CBCentralManager, didConnect peripheral: CBPeripheral) {
         delegate?.centralManager?(central, didConnect: peripheral)
     }
 
+    @available(OSX 10.7, *)
     func centralManager(_ central: CBCentralManager, didFailToConnect peripheral: CBPeripheral, error: Error?) {
         delegate?.centralManager?(central, didFailToConnect: peripheral, error: error)
     }
 
+    @available(OSX 10.7, *)
     func centralManager(_ central: CBCentralManager, didDisconnectPeripheral peripheral: CBPeripheral, error: Error?) {
         delegate?.centralManager?(central, didDisconnectPeripheral: peripheral, error: error)
     }
 }
 
 // This is a copy of CBCentralManagerDelegate without NSObjectProtocol conformance.
-@available(OSX 10.7, *)
 @objc protocol SwiftCBCentralManagerDelegate {
+    @available(OSX 10.7, *)
     func centralManagerDidUpdateState(_ central: CBCentralManager)
+
+    @available(OSX 10.7, *)
     @objc optional func centralManager(_ central: CBCentralManager, willRestoreState dict: [String : Any])
+
+    @available(OSX 10.7, *)
     @objc optional func centralManager(_ central: CBCentralManager, didDiscover peripheral: CBPeripheral,
                                        advertisementData: [String : Any], rssi RSSI: NSNumber)
+
+    @available(OSX 10.7, *)
     @objc optional func centralManager(_ central: CBCentralManager, didConnect peripheral: CBPeripheral)
+
+    @available(OSX 10.7, *)
     @objc optional func centralManager(_ central: CBCentralManager, didFailToConnect peripheral: CBPeripheral,
                                        error: Error?)
+
+    @available(OSX 10.7, *)
     @objc optional func centralManager(_ central: CBCentralManager, didDisconnectPeripheral peripheral: CBPeripheral,
                                        error: Error?)
 }
