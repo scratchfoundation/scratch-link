9f4bc14178ee646c74c6faafb74be586f24ee6f1
Christopher Willis-Ford <cwillisf@media.mit.edu>
2019-01-07 11:47:50 -0800

    Update convert-certificates.sh to read from CER file

--- Certificates/convert-certificates.sh
+++ Certificates/convert-certificates.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
+set -e
 
 ######
 
 # Inputs: if you don't know how to get these files, ask @cwillisf or @colbygk
-# - dm.pem (renamed from "dm-pem.txt") from "Device Manager Cert Bundle"
-# - scratch-device-manager.key from "Device Manager Key"
+# - Open "Device Manager Cert & Key"
+# - Save the certificate as "scratch-device-manager.cer"
+# - Save the key as "scratch-device-manager.key"
 
 # Output: various files in the "out" directory
 
@@ -13,33 +15,35 @@
 
 ######
 
-mkdir -p int out
+mkdir -p out
 
-# Extract each certificate from the PEM: ours, intermediate, CA
-if [ "`uname`" == "Darwin" ]; then
-	split -p "-----BEGIN CERTIFICATE-----" dm.pem int/cert-
-	mv int/{cert-aa,scratch-device-manager.pem}
-	mv int/{cert-ab,int.pem}
-	mv int/{cert-ac,ca.pem}
-else
-	csplit -f int/cert- dm.pem '/-----BEGIN CERTIFICATE-----/' '{2}'
-	rm int/cert-00 # empty
-	mv int/{cert-01,scratch-device-manager.pem}
-	mv int/{cert-02,int.pem}
-	mv int/{cert-03,ca.pem}
-fi
+# Code to split a PEM, in case a future version of the certificate goes back to PEM format:
+#if [ "`uname`" == "Darwin" ]; then
+#	split -p "-----BEGIN CERTIFICATE-----" dm.pem int/cert-
+#	mv int/{cert-aa,scratch-device-manager.pem}
+#	mv int/{cert-ab,int.pem}
+#	mv int/{cert-ac,ca.pem}
+#else
+#	csplit -f int/cert- dm.pem '/-----BEGIN CERTIFICATE-----/' '{2}'
+#	rm int/cert-00 # empty
+#	mv int/{cert-01,scratch-device-manager.pem}
+#	mv int/{cert-02,int.pem}
+#	mv int/{cert-03,ca.pem}
+#fi
 
 # Windows wants a single PFX containing the certificate along with its private key
 openssl pkcs12 \
 	-inkey scratch-device-manager.key \
-	-in int/scratch-device-manager.pem \
+	-in scratch-device-manager.cer \
 	-name "Scratch Link & Scratch Device Manager" \
 	-passout pass:Scratch \
 	-export -out out/scratch-device-manager.pfx
 
-# Perfect on Mac wants a single PEM containing the certificate and key
+# Perfect on Mac wants a single PEM containing the certificate and key along with the whole CA chain
 # Using grep this way enforces newlines between files
-grep -h ^ int/{scratch-device-manager,int,ca}.pem scratch-device-manager.key | tr -d '\r' > out/scratch-device-manager.pem
+grep -h ^ {scratch-device-manager,intermediate,certificate-authority}.cer scratch-device-manager.key \
+	| tr -d '\r' \
+	> out/scratch-device-manager.pem
 
 # Copy the PFX for the Windows build (the Mac Makefile "pulls" its certificates)
 cp -v out/scratch-device-manager.pfx ../Windows/scratch-link/Resources/
