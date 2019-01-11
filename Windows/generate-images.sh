579ea9bc00005c8aca564e209e213fac2d34cb91
Christopher Willis-Ford <cwillisf@media.mit.edu>
2019-01-10 17:06:16 -0800

    Add generate-images.sh output so contributors don't need Cygwin or WSL

--- Windows/generate-images.sh
+++ Windows/generate-images.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 # Windows users: please run this script using Cygwin or WSL
+# To do so, you may need to convert this file from Windows CRLF line endings to LF
 
 # Generate app icon using ImageMagick
 convert -verbose "../Assets/Windows/SVG/Windows Menu 400x400.svg" -define icon:auto-resize "./scratch-link/Resources/ScratchLink.ico"
