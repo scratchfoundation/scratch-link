9d87f21a1b9bfe153cc339557356b162e650779f
Christopher Willis-Ford <cwillisf@media.mit.edu>
2020-01-10 12:53:08 -0500

    improve compatibility of signtool step
    One caveat: the certificate used to sign the direct-download build must
    be stored in the LocalMachine certificate store now, rather than the
    CurrentUser store.

--- Windows/Import-WIN_CSC.ps1
+++ Windows/Import-WIN_CSC.ps1
@@ -5,8 +5,7 @@
     This command assumes two environment variables:
       - WIN_CSC_LINK is assumed to contain one or more Base64-encoded, encrypted digital certificate(s) and/or key(s).
       - WIN_CSC_KEY_PASSWORD is assumed to contain the password necessary to decrypt the data in WIN_CSC_LINK.
-        The script will import the contents of WIN_CSC_LINK into the current user's "Personal" certificate store or
-        that of the local machine, depending on permissions.
+        The script will import the contents of WIN_CSC_LINK into the local machine's "Personal" certificate store.
 .NOTES
     This is designed for use with a continuous integration service which places sensitive information, like digital
     certificates, into environment variables. It may not work well for other purposes.
@@ -22,13 +21,7 @@ try {
 
     $securePassword = (ConvertTo-SecureString -String $env:WIN_CSC_KEY_PASSWORD -AsPlainText -Force)
 
-    try {
-        Import-PfxCertificate -FilePath $tempPfx -Password $securePassword -CertStoreLocation "Cert:/CurrentUser/My"
-    } catch [System.Runtime.InteropServices.COMException] {
-        # Probably "Access Denied" above. It seems like "Run as Administrator" doesn't allow access to the user's
-        # certificate store, or something like that.
-        Import-PfxCertificate -FilePath $tempPfx -Password $securePassword -CertStoreLocation "Cert:/LocalMachine/My"
-    }
+    Import-PfxCertificate -FilePath $tempPfx -Password $securePassword -CertStoreLocation "Cert:/LocalMachine/My"
 }
 finally {
     Remove-Item -Force $tempPfx
