5422d1dee7449ef40ada40179d61e56576f6325a
Christopher Willis-Ford <cwillisf@media.mit.edu>
2020-04-15 15:51:37 -0700

    playground: support collapsing fieldsets

---
+++ playground/legend-collapse.js
@@ -0,0 +1,21 @@
+/*
+ * This field turns each fieldset legend into a button which collapses and expands the containing fieldset.
+ */
+
+window.addEventListener('load', function () {
+    console.log('document:load');
+    document.querySelectorAll('fieldset > legend').forEach(function (legendElement) {
+        function toggleCollapseFieldSet(mouseEvent) {
+            const legend = mouseEvent.currentTarget;
+            const fieldset = legend.parentElement;
+            if (fieldset.style.height) {
+                fieldset.style.overflow = null;
+                fieldset.style.height = null; // expand
+            } else {
+                fieldset.style.overflow = 'scroll';
+                fieldset.style.height = '3em'; // collapse
+            }
+        }
+        legendElement.addEventListener('click', toggleCollapseFieldSet);
+    });
+});
