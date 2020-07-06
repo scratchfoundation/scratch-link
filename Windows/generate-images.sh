a4f7261bd372e5c252fd507e7246a50d2d0750d7
Christopher Willis-Ford <cwillisf@media.mit.edu>
2020-07-06 16:37:15 -0700

    win: remove padding from APPX graphics

--- Windows/generate-images.sh
+++ Windows/generate-images.sh
@@ -4,5 +4,5 @@
 # To do so, you may need to convert this file from Windows CRLF line endings to LF
 
 # Generate app icon using ImageMagick
-convert -verbose "../Assets/Windows/SVG/Windows Menu 400x400.svg" -define icon:auto-resize "./scratch-link/Resources/ScratchLink.ico"
-convert -verbose "../Assets/Windows/SVG/Windows Tray 400x400.svg" -define icon:auto-resize "./scratch-link/Resources/NotifyIcon.ico"
+convert -verbose "../Assets/Windows/vector/Windows Menu 400x400.svg" -define icon:auto-resize "./scratch-link/Resources/ScratchLink.ico"
+convert -verbose "../Assets/Windows/vector/Windows Tray 400x400.svg" -define icon:auto-resize "./scratch-link/Resources/NotifyIcon.ico"
