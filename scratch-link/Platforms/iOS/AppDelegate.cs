400db0bb60d58e07a33dae36c7e2d115cf674496
Christopher Willis-Ford <7019101+cwillisf@users.noreply.github.com>
2022-03-14 10:09:28 -0700

    remove Android and iOS targets

--- scratch-link/Platforms/iOS/AppDelegate.cs
+++
@@ -1,9 +0,0 @@
-﻿using Foundation;
-
-namespace scratch_link;
-
-[Register("AppDelegate")]
-public class AppDelegate : MauiUIApplicationDelegate
-{
-	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
-}
