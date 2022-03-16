aeee85af4394a780d036741d79bd407c96f4b3a8
Christopher Willis-Ford <7019101+cwillisf@users.noreply.github.com>
2022-03-16 14:40:55 -0700

    add Safari helper app project

---
+++ Scratch Link Safari Helper/Scratch Link Safari Helper/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  Scratch Link Safari Helper
+//
+//  Created by Christopher Willis-Ford on 3/16/22.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        // Override point for customization after application launch.
+    }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+
+}
---
+++ Scratch Link Safari Helper/Scratch Link Safari HelperUITests/Scratch_Link_Safari_HelperUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Scratch_Link_Safari_HelperUITestsLaunchTests.swift
+//  Scratch Link Safari HelperUITests
+//
+//  Created by Christopher Willis-Ford on 3/16/22.
+//
+
+import XCTest
+
+class Scratch_Link_Safari_HelperUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
