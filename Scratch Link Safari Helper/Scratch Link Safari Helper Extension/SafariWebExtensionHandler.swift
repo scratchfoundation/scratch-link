7200442e64a6e11554565c1f968a928b58555d69
Christopher Willis-Ford <7019101+cwillisf@users.noreply.github.com>
2022-03-16 14:40:55 -0700

    add Safari helper app project

---
+++ Scratch Link Safari Helper/Scratch Link Safari Helper Extension/SafariWebExtensionHandler.swift
@@ -0,0 +1,26 @@
+//
+//  SafariWebExtensionHandler.swift
+//  Scratch Link Safari Helper Extension
+//
+//  Created by Christopher Willis-Ford on 3/16/22.
+//
+
+import SafariServices
+import os.log
+
+let SFExtensionMessageKey = "message"
+
+class SafariWebExtensionHandler: NSObject, NSExtensionRequestHandling {
+
+	func beginRequest(with context: NSExtensionContext) {
+        let item = context.inputItems[0] as! NSExtensionItem
+        let message = item.userInfo?[SFExtensionMessageKey]
+        os_log(.default, "Received message from browser.runtime.sendNativeMessage: %@", message as! CVarArg)
+
+        let response = NSExtensionItem()
+        response.userInfo = [ SFExtensionMessageKey: [ "Response to": message ] ]
+
+        context.completeRequest(returningItems: [response], completionHandler: nil)
+    }
+    
+}
