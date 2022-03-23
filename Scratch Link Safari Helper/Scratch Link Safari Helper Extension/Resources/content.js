d35292a98d26376aaebb1f462ec02188abdde307
Christopher Willis-Ford <7019101+cwillisf@users.noreply.github.com>
2022-03-23 15:29:52 -0700

    messy proof-of-concept for all the necessary comm paths

--- Scratch Link Safari Helper/Scratch Link Safari Helper Extension/Resources/content.js
+++ Scratch Link Safari Helper/Scratch Link Safari Helper Extension/Resources/content.js
@@ -1,7 +1,17 @@
-browser.runtime.sendMessage({ greeting: "hello" }).then((response) => {
-    console.log("Received response: ", response);
+// web page -> background script
+self.addEventListener("message", event => {
+    const message = event.data["to-scratch-link"];
+    if (message) {
+        console.log("content received from page: ", message);
+        browser.runtime.sendMessage(message).then(response => {
+            console.log("content response from background: ", response);
+            self.postMessage({"from-scratch-link": response}, event.origin);
+        });
+    }
 });
 
+// background script -> web page
 browser.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    console.log("Received request: ", request);
+    console.log("content received from background: ", request);
+    self.postMessage(request, self.origin);
 });
