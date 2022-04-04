ad6069993b65499855688672587f86c0cbf206d1
Christopher Willis-Ford <7019101+cwillisf@users.noreply.github.com>
2022-04-04 12:42:23 -0700

    make StyleCop happy

--- scratch-link/App.xaml.cs
+++ scratch-link/App.xaml.cs
@@ -1,11 +1,22 @@
-﻿namespace scratch_link;
+﻿// <copyright file="App.xaml.cs" company="Scratch Foundation">
+// Copyright (c) Scratch Foundation. All rights reserved.
+// </copyright>
 
+namespace ScratchLink;
+
+/// <summary>
+/// The <see cref="App"/> class contains the cross-platform entry point for the application.
+/// </summary>
 public partial class App : Application
 {
-	public App()
-	{
-		InitializeComponent();
+    /// <summary>
+    /// Initializes a new instance of the <see cref="App"/> class.
+    /// This is the cross-platform entry point.
+    /// </summary>
+    public App()
+    {
+        this.InitializeComponent();
 
-		MainPage = new MainPage();
-	}
+        this.MainPage = new MainPage();
+    }
 }
--- scratch-link/Platforms/Windows/App.xaml.cs
+++ scratch-link/Platforms/Windows/App.xaml.cs
@@ -1,9 +1,10 @@
-﻿using Microsoft.UI.Xaml;
+﻿// <copyright file="App.xaml.cs" company="Scratch Foundation">
+// Copyright (c) Scratch Foundation. All rights reserved.
+// </copyright>
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
-
-namespace scratch_link.WinUI;
+namespace ScratchLink.WinUI;
 
 /// <summary>
 /// Provides application-specific behavior to supplement the default Application class.
@@ -11,14 +12,18 @@ namespace scratch_link.WinUI;
 public partial class App : MauiWinUIApplication
 {
     /// <summary>
-    /// Initializes the singleton application object.  This is the first line of authored code
-    /// executed, and as such is the logical equivalent of main() or WinMain().
+    /// Initializes a new instance of the <see cref="App"/> class, the singleton application object. This is the
+    /// line of authored code executed, and as such is the logical equivalent of main() or WinMain().
     /// </summary>
     public App()
     {
         this.InitializeComponent();
     }
 
+    /// <summary>
+    /// Build and return a MauiApp instance to host our app on Windows.
+    /// MacCatalyst-specific configuration can go here.
+    /// </summary>
+    /// <returns>A new instance of <see cref="MauiApp"/> configured for our app.</returns>
     protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
 }
-
