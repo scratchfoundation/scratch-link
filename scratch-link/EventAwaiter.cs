bde6469d1b8865c6f3b9b69cd4e4f0a7ac5dd3df
Christopher Willis-Ford <7019101+cwillisf@users.noreply.github.com>
2022-05-26 15:00:56 -0700

    refactor EventAwaiter for better reliability
    This new structure better supports code which wants to await an event
    that it triggers. With the previous structure it was possible to miss
    the event, since the event could fire after the code which triggers it
    but before the event is hooked. This was especially problematic when
    debugging.
    
    Another bonus of this new structure is that it's possible to `await` the
    same event multiple times, even in a loop, without rebuilding the
    `EventAwaiter` and without any chance of missing events.

--- scratch-link/EventAwaiter.cs
+++ scratch-link/EventAwaiter.cs
@@ -5,107 +5,102 @@
 namespace ScratchLink;
 
 using System;
+using System.Diagnostics;
+using System.Threading.Channels;
 
 /// <summary>
 /// Adaptor to use "await" with events.
+/// Events which fire after construction but before calling <see cref="MakeTask"/> will be queued and can be retrieved later.
 /// </summary>
-public static class EventAwaiter
+/// <typeparam name="T">The type of EventArgs returned by this event.</typeparam>
+public class EventAwaiter<T> : IDisposable
 {
+    private readonly Action<EventHandler<T>> removeHandler;
+    private readonly Channel<T> events;
+    private bool disposedValue;
+
     /// <summary>
-    /// Make an awaitable Task which will resolve the next time this event is triggered.
+    /// Initializes a new instance of the <see cref="EventAwaiter{T}"/> class.
     /// This version only supports hooking an event in the calling class, since that's the only class which can access the implicit event delegate.
-    /// See <a href="https://stackoverflow.com/q/2560258">here</a> for more information and alternative solutions.
     /// </summary>
-    /// <typeparam name="T">The type of EventArgs.</typeparam>
-    /// <param name="targetEvent">The event to wait for.</param>
-    /// <param name="timeout">How long to wait for the event. If the timeout expires the Task will throw a <see cref="TimeoutException"/>.</param>
-    /// <param name="cancellationToken">The cancellation token to use to cancel the operation.</param>
-    /// <returns>A Task which will return the args passed to the event when it triggers.</returns>
-    public static Task<T> MakeTask<T>(EventHandler<T> targetEvent, TimeSpan timeout, CancellationToken cancellationToken)
+    /// <param name="targetEvent">The event to hook.</param>
+    public EventAwaiter(EventHandler<T> targetEvent)
+        : this(h => targetEvent += h, h => targetEvent -= h)
     {
-        return MakeTask<T>(
-            h => targetEvent += h,
-            h => targetEvent -= h,
-            timeout,
-            cancellationToken);
+    }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="EventAwaiter{T}"/> class.
+    /// This version supports hooking an event in another class by providing <c>addHandler</c> and <c>removeHandler</c> actions.
+    /// </summary>
+    /// <param name="addHandler">An action to add a handler to the event, like <c>handler => foo.MyEvent += handler</c>.</param>
+    /// <param name="removeHandler">An action to remove a handler from the event, like <c>handler => foo.MyEvent += handler</c>.</param>
+    public EventAwaiter(Action<EventHandler<T>> addHandler, Action<EventHandler<T>> removeHandler)
+    {
+        this.removeHandler = removeHandler;
+        this.events = Channel.CreateUnbounded<T>();
+        addHandler(this.EventHandler);
+    }
+
+    /// <summary>
+    /// Dispose of this <see cref="EventAwaiter{T}"/> instance, including ensuring that the event is no longer hooked.
+    /// </summary>
+    public void Dispose()
+    {
+        // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+        this.Dispose(disposing: true);
+        GC.SuppressFinalize(this);
     }
 
     /// <summary>
     /// Make an awaitable Task which will resolve the next time this event is triggered.
-    /// This version supports hooking an event in another class by providing <c>addHandler</c> and <c>removeHandler</c> actions.
     /// See <a href="https://stackoverflow.com/q/2560258">here</a> for more information and alternative solutions.
     /// </summary>
-    /// <typeparam name="T">The type of EventArgs.</typeparam>
-    /// <param name="addHandler">An action to add a handler to the event, like <c>handler => foo.MyEvent += handler</c>.</param>
-    /// <param name="removeHandler">An action to remove a handler from the event, like <c>handler => foo.MyEvent += handler</c>.</param>
     /// <param name="timeout">How long to wait for the event. If the timeout expires the Task will throw a <see cref="TimeoutException"/>.</param>
     /// <param name="cancellationToken">The cancellation token to use to cancel the operation.</param>
     /// <returns>A Task which will return the args passed to the event when it triggers.</returns>
-    public static Task<T> MakeTask<T>(Action<EventHandler<T>> addHandler, Action<EventHandler<T>> removeHandler, TimeSpan timeout, CancellationToken cancellationToken)
+    public ValueTask<T> MakeTask(TimeSpan timeout, CancellationToken cancellationToken)
     {
-        var completionSource = new TaskCompletionSource<T>();
-
         // localCancellationSource is canceled on cleanup and should not affect the task results.
         // If cancellationToken is cancelled it happed externally and should mark the task as canceled.
         var localCancellationSource = CancellationTokenSource.CreateLinkedTokenSource(cancellationToken);
+        localCancellationSource.CancelAfter(timeout);
+
         var localCancellationToken = localCancellationSource.Token;
 
-        EventHandler<T> handler = null;
-        CancellationTokenRegistration? tokenRegistration = null;
-        var delayTimer = Task.Delay(timeout, localCancellationToken);
+        return this.events.Reader.ReadAsync(localCancellationToken);
+    }
 
-        // make this safe to call multiple times in case (for example) a timeout goes into the event queue before a success finishes processing
-        var cleanup = () =>
+    /// <summary>
+    /// Implements the Dispose pattern for <see cref="EventAwaiter{T}"/>.
+    /// </summary>
+    /// <param name="disposing">True if called from <see cref="Dispose()"/>.</param>
+    protected virtual void Dispose(bool disposing)
+    {
+        if (!this.disposedValue)
         {
-            if (localCancellationSource?.IsCancellationRequested == false)
+            try
             {
-                localCancellationSource.Cancel();
-                localCancellationSource = null;
+                if (disposing)
+                {
+                    // TODO: dispose managed state (managed objects)
+                    this.removeHandler(this.EventHandler);
+                }
             }
-
-            if (handler != null)
+            finally
             {
-                removeHandler(handler);
-                handler = null;
+                // TODO: free unmanaged resources (unmanaged objects) and override finalizer
+                // TODO: set large fields to null
+                this.disposedValue = true;
             }
+        }
+    }
 
-            if (delayTimer != null)
-            {
-                delayTimer.Dispose();
-                delayTimer = null;
-            }
-
-            if (tokenRegistration != null)
-            {
-                tokenRegistration?.Dispose();
-                tokenRegistration = null;
-            }
-        };
-
-        // success
-        handler = (object sender, T args) =>
+    private void EventHandler(object sender, T args)
+    {
+        if (!this.events.Writer.TryWrite(args))
         {
-            cleanup();
-            completionSource.TrySetResult(args);
-        };
-        addHandler(handler);
-
-        // timeout
-        delayTimer.ContinueWith(
-            _ =>
-            {
-                cleanup();
-                completionSource.TrySetException(new TimeoutException());
-            },
-            localCancellationToken);
-
-        // cancel
-        tokenRegistration = cancellationToken.Register(() =>
-        {
-            cleanup();
-            completionSource.TrySetCanceled();
-        });
-
-        return completionSource.Task;
+            Debug.Print("EventAwaiter failed to write an event!");
+        }
     }
 }
