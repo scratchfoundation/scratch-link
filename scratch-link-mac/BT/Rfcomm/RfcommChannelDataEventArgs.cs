00e616ee0fdac0f42d4bfc59b69d989d8fdee878
Christopher Willis-Ford <7019101+cwillisf@users.noreply.github.com>
2022-07-18 16:36:33 -0700

    Mac BT: implement 'connect', remove 'ouiPrefix'

---
+++ scratch-link-mac/BT/Rfcomm/RfcommChannelDataEventArgs.cs
@@ -0,0 +1,18 @@
+﻿// <copyright file="RfcommChannelDataEventArgs.cs" company="Scratch Foundation">
+// Copyright (c) Scratch Foundation. All rights reserved.
+// </copyright>
+
+namespace ScratchLink.Mac.BT.Rfcomm;
+
+using System;
+
+/// <summary>
+/// Event args for receiving data through an RFCOMM channel.
+/// </summary>
+public class RfcommChannelDataEventArgs : RfcommChannelEventArgs
+{
+    /// <summary>
+    /// Gets or sets the data received through the RFCOMM channel.
+    /// </summary>
+    public byte[] Data { get; set; }
+}
