a882ff0cb6557b0161876f332c1d4f01de3f9e72
Christopher Willis-Ford <7019101+cwillisf@users.noreply.github.com>
2022-07-21 13:51:19 -0700

    Mac: remove unnecessary 'main' window
    Also, enable `LSUIElement` in `Info.plist`

--- scratch-link-mac/Main.cs
+++ scratch-link-mac/Main.cs
@@ -14,6 +14,7 @@ internal static class MainClass
     private static void Main(string[] args)
     {
         NSApplication.Init();
+        NSApplication.SharedApplication.Delegate = new AppDelegate();
         NSApplication.Main(args);
     }
 }
