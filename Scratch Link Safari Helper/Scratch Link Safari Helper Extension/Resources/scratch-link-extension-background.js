b32ee4e643b301cede127ea1ee21117bf2156c41
Christopher Willis-Ford <7019101+cwillisf@users.noreply.github.com>
2022-08-22 08:22:43 -0700

    chore(webextension): add comments to the background script

--- Scratch Link Safari Helper/Scratch Link Safari Helper Extension/Resources/scratch-link-extension-background.js
+++ Scratch Link Safari Helper/Scratch Link Safari Helper Extension/Resources/scratch-link-extension-background.js
@@ -1,17 +1,21 @@
 
+// this ID is ignored: the background script is only allowed to communicate with its associated app
 const appId = 'application.id';
 
-// map of session ID to {tabId, options} where options = {frameId}.
+// map of session ID to {tabId, options} where options = {frameId}
 // see also browser.tabs.sendMessage
 const sessionTabMap = new Map();
 
+// handle a message from a content script
 browser.runtime.onMessage.addListener((request, sender, sendResponse) => {
+    // the content script reports that its window / tab / frame is being unloaded
     if (request === 'unload') {
         return onSenderUnload(sender);
     }
 
     const {session, method, params, id} = request;
 
+    // forward the message to the native app
     browser.runtime.sendNativeMessage(appId, {
         session,
         method,
@@ -23,10 +27,12 @@ browser.runtime.onMessage.addListener((request, sender, sendResponse) => {
         }
         sendResponse(response);
     });
-    // asynchronous response expected?
+
+    // if the message contained an ID, tell the browser we're expecting an asynchronous call to sendResponse
     return (id !== undefined);
 });
 
+// connect a port to receive messages from the native app
 const port = browser.runtime.connectNative(appId);
 port.onMessage.addListener(message => {
     const clientInfo = sessionTabMap.get(message.userInfo.session);
@@ -35,21 +41,26 @@ port.onMessage.addListener(message => {
     }
 });
 
+// handle a connection coming from a content script
 browser.runtime.onConnect.addListener(port => {
+    // handle a message from the content script session associated with this port
     port.onMessage.addListener(messageToScratchLink => {
         const {session, method, params, id} = messageToScratchLink;
 
+        // forward the message to the native app (which will forward it to Scratch Link)
         browser.runtime.sendNativeMessage(appId, {
             session,
             method,
             params,
             id
         }, responseFromScratchLink => {
+            // send the native app's response back to the content script session
             port.postMessage(responseFromScratchLink);
         } );
     } );
 });
 
+// handle a client unloading
 const onSenderUnload = sender => {
     sessionTabMap.forEach((clientInfo, sessionId) => {
         if (sender.tab.id != clientInfo.tabId || sender.frameId != clientInfo.options.frameId) {
