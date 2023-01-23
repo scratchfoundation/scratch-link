05d2866f2bca7f3bee8af67e0769458b7c4399e9
Christopher Willis-Ford <7019101+cwillisf@users.noreply.github.com>
2023-01-23 15:50:56 -0800

    feat(windows): build and run ScratchApp, receive WS connections

---
+++ scratch-link-win/ScratchLinkWindowsApp.cs
@@ -0,0 +1,35 @@
+﻿// <copyright file="ScratchLinkWindowsApp.cs" company="Scratch Foundation">
+// Copyright (c) Scratch Foundation. All rights reserved.
+// </copyright>
+
+namespace ScratchLink.Win;
+
+using ScratchLink.Win.BLE;
+using System.Diagnostics;
+
+/// <summary>
+/// Windows application context to hold the <c>ScratchLinkApp</c> instance.
+/// </summary>
+public class ScratchLinkWindowsApp : ApplicationContext
+{
+    private ScratchLinkApp app;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="ScratchLinkWindowsApp"/> class.
+    /// </summary>
+    /// <param name="args">Arguments passed from the command line.</param>
+    public ScratchLinkWindowsApp(string[] args)
+    {
+        // Trace.Listeners.Add(??);
+        Trace.WriteLine("Starting...");
+
+        var appBuilder = new ScratchLinkApp.Builder();
+        appBuilder.SetArguments(args);
+        appBuilder.SetSessionManager<WinSessionManager>();
+        appBuilder.SetGattHelpers<WinGattHelpers, Guid>();
+
+        this.app = appBuilder.Build();
+
+        this.app.Run();
+    }
+}
