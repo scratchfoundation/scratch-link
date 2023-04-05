964b17fb8183126d108842cbf2b6ccffe8c35530
Christopher Willis-Ford <7019101+cwillisf@users.noreply.github.com>
2023-04-05 10:05:27 -0700

    build: add and configure semantic-release

---
+++ release.config.js
@@ -0,0 +1,32 @@
+module.exports = {
+    branches: [
+        "main",
+        {
+            name: "develop",
+            prerelease: true
+        }
+    ],
+    plugins: [
+        '@semantic-release/commit-analyzer',
+        '@semantic-release/release-notes-generator',
+        [
+            '@semantic-release/changelog',
+            {
+                changelogTitle: '# Changelog\n\nAll notable changes to this project will be documented in this file. See\n[Conventional Commits](https://conventionalcommits.org) for commit guidelines.'
+            }
+        ],
+        [
+            '@semantic-release/git',
+            {
+                // eslint-disable-next-line no-template-curly-in-string
+                message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}'
+            }
+        ],
+        [
+            '@semantic-release/github',
+            {
+                assets: 'pack/*.tgz'
+            }
+        ]
+    ]
+};
